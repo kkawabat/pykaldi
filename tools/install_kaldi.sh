8a528b467f67b7f4db8bc8ef04c12e2262b1902e
kkawabat <kkawabat@auralanalytics.com>
2020-04-08 14:50:01 -0700

    Readded all the llvm files. started working on implementing the slimmed version of pykaldi into a dockerfile
    for some reason version control insists on ignoring the llvm directory whenever I tried to add it.

--- tools/install_kaldi.sh
+++ tools/install_kaldi.sh
@@ -23,10 +23,15 @@ touch "python/.use_default_python"
 
 ./extras/check_dependencies.sh
 
-make -j $(nproc)
+# removes libraries that wont be used after installation for memory
+make -j $(nproc) openfst OPENFST_CONFIGURE="--enable-shared --enable-bin --disable-dependency-tracking"
+find . -type d -name "openfst*" -exec rm -rf {}/src/script/.libs \;
 
 cd ../src
 ./configure --shared --use-cuda=no
 make clean -j $(nproc) && make depend -j $(nproc) && make -j $(nproc) base matrix util feat tree gmm transform fstext hmm lm decoder lat kws
+
+find . -name "*.a" -delete
+find . -name "*.o" -delete
 
 echo "Done installing Kaldi."
